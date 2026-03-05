d801e1840f565097d41e4f65dd6c7a8613fd3eb4
mashkioya <5342078+mashkioya@users.noreply.github.com>
2026-03-05 07:34:06 -0600

    added option2

--- src/script.js
+++ src/script.js
@@ -6,4 +6,19 @@ document.addEventListener('click', function (e) {
   const panel = document.getElementById(targetId);
 
   panel.classList.add('open');
+  
+  // pérate tantito
+  setTimeout(() => { 
+    // disable clicked link
+    btn.classList.add('disabled'); 
+  }, 500);
+
+  // hide sibling buttons
+  const siblings = btn.parentElement.querySelectorAll('a');
+
+  siblings.forEach(a => {
+    if(a !== btn) {
+      a.classList.add('hidden');
+    }
+  });
 });
